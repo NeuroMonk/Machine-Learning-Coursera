have started implement ex3, and fix normalization in ex2 cost function

ex2/test_cases.py
@@ -1,8 +1,28 @@
 import numpy as np
+import sys
 from sigmoid import sigmoid
+from costFunctionReg import costFunctionReg
 
-print sigmoid(12000000) # ans 1
-print sigmoid(-25000) # ans 0
-print sigmoid(0) # ans 0.5
-print sigmoid(np.array([4,5,6])) # ans 0.9820 0.9933 0.9975
+if __name__ == "__main__":
 
+    magic3 = np.array([[8, 1, 6], 
+                       [3, 5, 7],
+                       [4, 9, 2]])
+    
+    X = np.concatenate( (np.ones((3, 1)),  magic3), axis = 1)
+    y = np.array([[1, 0, 1]]).T
+    theta = np.array([-2, -1, 1, 2])
+    
+    try:
+        assert( sigmoid(12000000) == 1)
+        assert( sigmoid(-25000) == 0)
+        assert( sigmoid(0) == 0.5)
+        assert( np.around(sigmoid(np.array([4, 5, 6])), decimals = 4).tolist() == [0.9820, 0.9933, 0.9975] )
+    
+        assert(np.around( costFunctionReg(X, y, theta, 0), decimals=4).tolist() == [4.6832])
+        assert(np.around( costFunctionReg(X, y, theta, 3), decimals=4).tolist() == [7.6832])
+        
+    except:
+        sys.exit("Unit test failed")
+    
+    sys.exit(0)

ex3/displayData.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def displayData(X, example_width=None):
 
@@ -26,11 +27,14 @@ def displayData(X, example_width=None):
                 break
 
             max_val = np.max(np.abs(X[curr_ex, :]))
-            display_array[pad + (j - 1) * (example_height + pad) + range(1, example_height),\
-                          pad + (i - 1) * (example_width + pad) + range(1, example_width)] =\
-                          np.reshape(X[curr_ex, :], example_height, example_width) / max_val
+            display_array[np.ix_(pad + (j - 1) * (example_height + pad) + range(example_height),\
+                          pad + (i - 1) * (example_width + pad) + range(example_width))] =\
+                          X[curr_ex, :].reshape(example_height, example_width) / max_val
             
             curr_ex = curr_ex + 1
 
         if curr_ex>m:
             break
+
+    plt.imshow(display_array.T, cmap=plt.cm.gray)
+    plt.show()

ex3/ex3.py
@@ -10,6 +10,18 @@ data = sio.loadmat("ex3data1.mat")
 X = np.array(data['X'])
 y = np.array(data['y'])
 
-displayData(X)
+input_layer_size = 400;
+num_labels = 10;
+
+m, n = X.shape
+l = 0.1;
+
+
+rand_indices =  np.random.permutation(m)
+sel = X[rand_indices[0:101], :]
+
+displayData(sel)
+
+
 
 

ex3/lrCostFunction.py
@@ -2,19 +2,17 @@ from __future__ import division
 import numpy as np
 from sigmoid import sigmoid
 
-
-def costFunctionReg(X, y, theta, l):
+def lrCostFunction(X, y, theta, l):
 
     theta = theta[np.newaxis, : ]
 
     J = 0
     m = len(y)
     h = sigmoid(np.dot(X, theta.T));
-    J = ( (-1/m) * np.sum( np.dot(y.T, np.log(h)) + np.dot((1 - y.T), np.log(1-h))  ) ) 
-    #should not regulirize theta(0), 
+    J = ( (-1/m) * np.sum( np.dot(y.T, np.log(h)) + np.dot((1 - y.T), np.log(1-h))  ) )
     reg = (l/(2*m) * sum(np.delete(theta.T, 0, 0)  ** 2))
     J = J + reg   
 
-    #grad = (1/m) * np.sum((h - y) * X, axis = 0)
-
+    #grad = (1/m) * np.sum((h - y) * X, axis = 0) + (l/m) * theta
+   
     return J